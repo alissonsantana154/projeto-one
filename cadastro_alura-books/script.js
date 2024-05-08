19eb2aa7d5eeee6e04dfc951459070233906e9cb
Alisson Santana <alissonsantana16608@gmail.com>
2024-05-08 13:21:57 -0300

    feat: Criando funcoes assincronas

--- cadastro_alura-books/script.js
+++ cadastro_alura-books/script.js
@@ -1,13 +1,22 @@
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if (r.erro) {
-        throw Error('esse cep não existe!')
-    } else {
-        console.log(r)
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
     }
-})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluido!'))
+}
 
-console.log(consultaCEP)
+/* --- Lidando com várias requisições ao mesmo tempo com Promise.all ---
+
+let ceps = ['01001000', '01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+console.log(conjuntoCeps);
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+
+*/
