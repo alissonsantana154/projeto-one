bdbf8083cdbd98efabcd169cff17d60e3a46d3dc
Alisson Santana <alissonsantana16608@gmail.com>
2024-05-09 14:17:14 -0300

    feat: Requisição POST, Captando eventos, Enviar videos

--- aluraplay-requisicoes/js/mostrarVideos.js
+++ aluraplay-requisicoes/js/mostrarVideos.js
@@ -1,3 +1,4 @@
+
 import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]");
@@ -5,13 +6,13 @@ const lista = document.querySelector("[data-lista]");
 function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement("li");
     video.className = "videos__item";
-    video.innerHTML = `
-    <iframe width="100%" height="72%" src="${url}"
-    title="${titulo} frameborder="0"
+    video.innerHTML = 
+    `<iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
     <div class="descricao-video">
-    <img src="${imagem}">
+    <img src="${imagem}" alt="logo canal alura">
     <h3>${titulo}</h3>
     <p>${descricao}</p>
     </div>`
@@ -21,7 +22,8 @@ function constroiCard(titulo, descricao, url, imagem) {
 
 async function listaVideos() {
     const listaApi = await conectaApi.listaVideos();
-    listaApi.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+    listaApi.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
 }
 
 listaVideos();
