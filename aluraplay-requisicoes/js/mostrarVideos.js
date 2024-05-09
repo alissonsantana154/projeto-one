33770018e23f53c87d8a32594081f23677d1c555
Alisson Santana <alissonsantana16608@gmail.com>
2024-05-09 14:45:31 -0300

    feat: Busca de vídeos, Busca Dinâmica, Reutilizar funções, Listas filtrada

--- aluraplay-requisicoes/js/mostrarVideos.js
+++ aluraplay-requisicoes/js/mostrarVideos.js
@@ -1,13 +1,11 @@
-
 import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function constroiCard(titulo, descricao, url, imagem) {
+export default function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement("li");
     video.className = "videos__item";
-    video.innerHTML = 
-    `<iframe width="100%" height="72%" src="${url}"
+    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
     title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
