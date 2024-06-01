f534e665855ffc02ae2704ecb59899fb68845b73
Alisson Santana <alissonsantana16608@gmail.com>
2024-06-01 14:28:09 -0300

    Lista dos times

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,7 +1,18 @@
 import CampoTexto from "../CampoTexto"
+import ListaSuspensa from "../ListaSuspensa"
 import "./Formulario.css"
 
 const Fomulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Back-End",
+    "Full-Stack",
+    "Mobile",
+    "UI/UX",
+    "Analista de Testes",
+  ]
+
   return (
     <section className="formulario">
       <form>
@@ -9,6 +20,7 @@ const Fomulario = () => {
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
       </form>
     </section>
   )
