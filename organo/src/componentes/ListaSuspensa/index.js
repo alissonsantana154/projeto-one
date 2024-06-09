7f0059cef6729427c170583fc344ba404e7b5b3d
Alisson Santana <alissonsantana16608@gmail.com>
2024-06-09 20:52:29 -0300

    Corrigindo os bugs

--- organo/src/componentes/ListaSuspensa/index.js
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -6,8 +6,10 @@ const ListaSuspensa = (props) => {
       <label>{props.label}</label>
       <select
         onChange={(evento) => props.aoAlterado(evento.target.value)}
-        required={props.obrigatorio}
+        required={props.required}
+        value={props.valor}
       >
+        <option value=""></option>
         {props.itens.map((item) => {
           return <option key={item}>{item}</option>
         })}
